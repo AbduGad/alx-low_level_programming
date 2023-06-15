4b02fe87a9f1c7563781f945b621546a23e44989
AbduGad <abdo79gad@gmail.com>
2023-06-15 21:47:39 +0300

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *main - printing using printf not puts
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+
