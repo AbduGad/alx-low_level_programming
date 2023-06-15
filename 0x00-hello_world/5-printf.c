37730a4696f0b2e481bf5bfffab6224c99b803ea
AbduGad <abdo79gad@gmail.com>
2023-06-15 21:49:06 +0300

    5edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,15 +2,14 @@
 
 
 /**
- *
- *main - printing using printf not puts
+ * main - printing using printf not puts
  *
  *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }
 
