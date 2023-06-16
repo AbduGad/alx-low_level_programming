8440c8573cf2510a48fc71fad28cb60e3275775e
AbduGad <abdo79gad@gmail.com>
2023-06-16 22:53:24 +0300

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - 3 combinations
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i < 8)
+	{
+		j = i + 1;
+		while (j < 9)
+		{
+			t = j + 1;
+			while (t < 10)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(t + '0');
+				if (i != 7 || j != 8 || t !=9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				t++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
