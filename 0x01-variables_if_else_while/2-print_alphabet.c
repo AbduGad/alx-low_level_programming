0f4467654e342162bae659bd52af550673a22c03
AbduGad <abdo79gad@gmail.com>
2023-06-16 16:30:38 +0300

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets;
+
+	alphabets = 'a';
+	while (alphabets <= z)
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	putchar('\n');
+	return (0);
+
+}
