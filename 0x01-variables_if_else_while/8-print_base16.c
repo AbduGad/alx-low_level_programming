b363c050c4c225f11172667fa58fee44d425b645
AbduGad <abdo79gad@gmail.com>
2023-06-16 18:05:14 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - numbers of base 16 lower case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
