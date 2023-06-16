955e2fb18ac403f705b543b15573547fb1b62c6e
AbduGad <abdo79gad@gmail.com>
2023-06-16 16:17:57 +0300

    1 fixed spaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,20 +12,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
 	/**
 	 * main - function to print last digit of n
 	 *
 	 * Return: 0
 	 */
-	end = n%10;
+	end = n % 10;
 
 	if (end > 5)
-		printf("Last digit of %i is %i and is greater than 5\n" , n, end);
+		printf("Last digit of %i is %i and is greater than 5\n", n, end);
 	else
 		if (end == 0)
-			printf("Last digit of %i is %i and is 0\n" , n, end);
-		else if ( end < 6)
+			printf("Last digit of %i is %i and is 0\n", n, end);
+		else if (end < 6)
 			printf("Last digit of %i is %i and is less than 6 and not 0\n", n, end);
 
 	return (0);
