06a612c7ac729d1e54a6d71f62a7b49036ca6e0a
AbduGad <abdo79gad@gmail.com>
2023-06-16 15:50:58 +0300

    0 fix added comment for main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - getting random number and checking posi or negi or 0
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
