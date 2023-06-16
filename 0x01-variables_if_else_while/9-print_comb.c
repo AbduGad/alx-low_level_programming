d74c3f8e7a8af1bcf9e89839a3675e3a867e9757
AbduGad <abdo79gad@gmail.com>
2023-06-16 18:16:55 +0300

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations of 1 digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	
+	while(i < 10)
+	{
+		if (i == 9)
+			putchar(i + '$');
+		
+		putchar(i + ',' + ' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
