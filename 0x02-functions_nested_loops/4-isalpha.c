01da4661a4e23018ad922e7903ee4e7907333e5a
AbduGad <abdo79gad@gmail.com>
2023-06-20 14:54:35 +0300

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if char is a lower/upper case or otherwise
+ * @c: input 
+ * Return: 1 if true 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
