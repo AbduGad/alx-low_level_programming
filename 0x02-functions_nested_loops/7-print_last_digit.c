e28ac93f83f57230e5b82667fd6d6cfd810ed38b
AbduGad <abdo79gad@gmail.com>
2023-06-20 15:27:59 +0300

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,14 @@
 /**
  * print_last_digit - "sigh"
  * @x: input
- * 
+ *
  * Return: last digit
  */
 
 int print_last_digit(int x)
 {
-	return (x % 10);
+	if (x < 0)
+		return (x % 10);
+	else
+		return ((x * -1) % 10);
 }
