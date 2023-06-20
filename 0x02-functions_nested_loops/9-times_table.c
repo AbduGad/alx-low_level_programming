22cb55ef606be118b15bdfc415ecf2a610f75740
AbduGad <abdo79gad@gmail.com>
2023-06-20 18:51:06 +0300

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * not input void
+ * Return: void
+ */
+
+void time_table(void)
+{
+	int i = 0;
+	int j;
+
+	while ()
+	{
+		j = 0;
+		while (j < 10)
+		{
+			_putchar((i * j) + '0');
+			_putchar(',');
+			_putchar(' ');
+			if ((i * j) < 10)
+				_putchar(' ');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
