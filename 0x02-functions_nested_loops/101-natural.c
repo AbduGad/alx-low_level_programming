9126956a81035e47c061ad76993daef476a48251
AbduGad <abdo79gad@gmail.com>
2023-06-21 14:47:05 +0300

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void main(void)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+}
