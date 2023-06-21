5ac0d31dbe42b8432c50f6e84282643e7392d149
AbduGad <abdo79gad@gmail.com>
2023-06-21 21:12:12 +0300

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even nums in fabonacci sequence
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	long cn = 1;
+	long pn = 0;
+	long nn;
+	long sum = 0;
+
+	while (cn <= 4000000)
+	{
+		nn = pn + cn;
+		pn = cn;
+		cn = nn;
+		if ((cn % 2) == 0)
+		{
+			sum += cn;
+		}
+	}
+	printf("%.0f\n", sum);
+	return (0);
+}
