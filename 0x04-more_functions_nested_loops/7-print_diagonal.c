2f1d56f1a0ee6cc3d085fbc9b5c20dc35837ee46
AbduGad <abdo79gad@gmail.com>
2023-06-22 17:36:55 +0300

    04 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_diagonal - its in the name
+* @n: input
+*
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			for (i = 1; i < j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
