bb46dd10b579467bfc179e7b1a91438bd4c1a172
AbduGad <abdo79gad@gmail.com>
2023-06-22 18:08:56 +0300

    04 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prints from 0 to 100 for %3 type fizz %5 buzz % both fizzbuzz
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 && i % 5)
+			printf("FizzBuzz ");
+		else if (i % 5)
+			printf("Buzz ");
+		else if (i % 3)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
