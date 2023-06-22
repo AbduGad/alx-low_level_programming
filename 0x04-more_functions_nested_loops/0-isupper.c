7fca1fe9b37a2fbd635a6b7acf56e6bb99e8c1b9
AbduGad <abdo79gad@gmail.com>
2023-06-22 14:17:46 +0300

    more loops 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isupper - checks upper
+*
+* Return: 0
+*/
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		return (0);
+	}
+	else
+		return (2);
+}
