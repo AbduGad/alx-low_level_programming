4b87e96367f773323c036e985da7d230834596f1
AbduGad <abdo79gad@gmail.com>
2023-06-29 22:24:49 +0300

    06 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* reverse_array - in the name
+* @a: array
+* @n: array size
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+	}
+}
