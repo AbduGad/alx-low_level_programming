af2c1bba69700771dfc5da5af2f6495e6bb9ab4f
AbduGad <abdo79gad@gmail.com>
2023-07-03 19:02:03 +0300

    07 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+*_strchr - search for character in string
+*@s: string
+*@c: character
+*Return: pointer to the char in the string s
+*/
+
+char *_strchr(char *s, char c)
+{
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (NULL);
+}
