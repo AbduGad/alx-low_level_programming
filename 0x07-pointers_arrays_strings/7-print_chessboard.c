fe4790b1fb805a2244ed7e18f2a22bdf93435001
AbduGad <abdo79gad@gmail.com>
2023-07-04 18:47:37 +0300

    07 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; (sizeof(a[i]) / sizeof(a[i][j])); i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; a[i][j]; j++)
+		for (j = 0; j < (sizeof(a[i]) / sizeof(a[i][j]));; j++)
 		{
 			_putchar(a[i][j]);
 		}
