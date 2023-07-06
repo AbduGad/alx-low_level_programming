c5565a702c2d74449f7c2ae412495c7d355b19d5
AbduGad <abdo79gad@gmail.com>
2023-07-06 18:29:30 +0300

    07 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,26 +9,23 @@
 
 char *_strstr(char *hay, char *needle)
 {
-	char *p;
-	int i, j = 0, b = 0;
-
+	char *p = "NULL";
+	int i, j = 0;
+    
+    if (!*needle)
+        return(p = hay);
 	for (i = 0; hay[i] && needle[j]; i++)
 	{
 		if (hay[i] == needle[0])
 		{
-			b = 1;
 			p = &hay[i];
 		}
 		if (hay[i] == needle[j])
 			j++;
 		else
 		{
-			b = 0;
 			j = 0;
 		}
 	}
-	if (b == 1)
-		return (p);
-	else
-		return (0);
+	return (p);
 }
