a93efe08132460ac6c981c3ae479cdbfe8d63e3f
AbduGad <abdo79gad@gmail.com>
2023-07-08 20:04:43 +0300

    0A 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all args
+ *
+ * @argc: input
+ * @argv: input
+ * @Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    while (*argv)
+        printf("%s\n", argv[i++]);
+    return (0);
+}
