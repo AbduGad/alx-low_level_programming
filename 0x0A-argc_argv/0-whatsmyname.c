5034d7cbd0be1589adbe88559f1e44cbce2f31d2
AbduGad <abdo79gad@gmail.com>
2023-07-08 17:16:02 +0300

    0A 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+* main - test argv
+* @argc:  size of argv
+* @argv: pointer ti array of char
+* Return: 0
+*/
+int main(int argc, char *argv[])
 {
 	int i;
 
