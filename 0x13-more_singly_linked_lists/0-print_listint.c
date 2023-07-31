96290bfe767652f02506b0d5191a8ed8d1b61d0d
AbduGad <abdo79gad@gmail.com>
2023-07-31 19:31:34 +0300

    013 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <lists.h>
+/**
+ * print_listint - prints whats inside each part of list 
+ * @h: pointer to list head
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t size = 0;
+
+    while (h != NULL)
+    {
+        printf("%i\n", h->n);
+        size++;
+        h = h->next;
+    }
+    printf("-> %i elements", size);
+    return (size);
+}
