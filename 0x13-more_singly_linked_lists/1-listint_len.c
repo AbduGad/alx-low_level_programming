383a3806468f00ef496a4904bee56aeefde9d720
AbduGad <abdo79gad@gmail.com>
2023-07-31 19:47:26 +0300

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - counts list length
+ * @h: pointer to head of list
+ * Return: list length
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t size = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        size++;
+    }
+    return (size);
+}
