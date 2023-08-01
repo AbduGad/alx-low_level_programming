4388d69a3a11a1060f560f55577e5331401e766f
AbduGad <abdo79gad@gmail.com>
2023-08-01 15:42:23 +0300

    013 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees lintint_t
+ * @head: head pointer to the list 
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *holder;
+
+    while (head != NULL)
+    {
+        holder = head->next;
+        free(holder);
+        head = holder;
+    }
+}
