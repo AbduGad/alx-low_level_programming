15531991a7d561e39ef80b0d9c38e7cc042afccb
AbduGad <abdo79gad@gmail.com>
2023-08-01 21:48:52 +0300

    test

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
     while (myindex != index)
     {
         myindex++;
-        if (holder->next == NULL)
+        if (holder == NULL)
             return (NULL);
         holder = holder->next;
     }
