c63e550d01a60bdbfe5328fcd1531fea1ce7f311
AbduGad <abdo79gad@gmail.com>
2023-08-02 22:14:18 +0300

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int myindex = 0;
 	listint_t *holder, *new;
 
+	if (head == NULL)
+		return (NULL);
 	while (myindex != idx - 1)
 	{
 		if (!*head)
