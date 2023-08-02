c80710a2350be249d23e6fbb680bb2f49e6671fd
AbduGad <abdo79gad@gmail.com>
2023-08-03 00:00:57 +0300

    t

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (myindex != idx - 1)
 		{
-			if (!*head || !(*head)->next)
+			if (!*head)
 				return (NULL);
 			*head = (*head)->next;
 		}
