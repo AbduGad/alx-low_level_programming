5459c4c154e64ddbe32c794a7d36f5c5aab537b9
AbduGad <abdo79gad@gmail.com>
2023-08-03 00:14:45 +0300

    t

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,8 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		new->next = temp;
-		temp = new;
+		new->next = *head;
+		*head = new;
 	}
 	return (new);
 }
