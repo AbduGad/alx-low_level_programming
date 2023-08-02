b64f5f8fd7bf9754dc63bee90b3f18c55c0e5729
AbduGad <abdo79gad@gmail.com>
2023-08-02 23:21:29 +0300

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (head == NULL || *head == NULL && idx != 0)
+	if (head == NULL)
 		return (NULL);
 	
 	if (idx > 0)
