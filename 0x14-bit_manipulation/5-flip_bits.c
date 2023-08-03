1ac1e87ea80553133c3ab8358e33200da276ceb9
AbduGad <abdo79gad@gmail.com>
2023-08-04 01:20:48 +0300

    014 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns the number of bits you would need to flip
+ * @n: number
+ * @m:
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    int shifts = 0, counter = 0;
+    unsigned long int c = (n > m) ? (n) : (m);
+
+    /*printf("c -%lu shifts - %lu\n", c, c >> 10);*/
+    while (c)
+    {
+        c >>= 1;
+        shifts++;
+    }
+    shifts--;
+    /*printf("c -%lu shifts - %lu\n", c, n >> shifts);*/
+    while (shifts >= 0)
+    {
+        if ((n & (1 << shifts)) != (m & (1 << shifts)))
+            counter++;
+        shifts--;
+    }
+    return (counter);
+}
---
+++ 0x14-bit_manipulation/5-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned int n;
+
+    n = flip_bits(1024, 1);
+    printf("%u\n", n);
+    n = flip_bits(402, 98);
+    printf("%u\n", n);
+    n = flip_bits(1024, 3);
+    printf("%u\n", n);
+    n = flip_bits(1024, 1025);
+    printf("%u\n", n);
+    return (0);
+}
+//001100010
+//110010010
+
+//00000000001
+//10000000000
+
+//10000000000
+//00000000011
+
+//01000000000-0
+//01000000000-1
