da61923b547f08cce34556d9c589161e1036d3fd
AbduGad <abdo79gad@gmail.com>
2023-08-03 23:49:40 +0300

    test

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,15 +9,16 @@ void print_binary(unsigned long int n)
     unsigned long int copy = n, res;
     int shifts = 0;
 
+    if (n == 0)
+        _putchar(0 +'0');
     while (copy > 0)
     {
         shifts++;
         copy >>= 1;
     }
-    if (shifts == 0)
-        _putchar(0 +'0');
-    while(--shifts > 0)
+    while(shifts > 0)
     {
+        shifts--;
         res = (n >> shifts) & 1;
         _putchar(res + '0');
     }
