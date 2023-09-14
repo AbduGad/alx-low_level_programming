38eb7c18819a8a4c46c32b754941e38e13d7aba8
AbduGad <abdo79gad@gmail.com>
2023-09-14 15:29:05 +0300

    017 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,34 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
- * dlistint_len - counts the elements in a linked list
- * 
- * @h: head of the list
+ * add_dnodeint - counts the elements in a linked list
+ * @head: head of the list
+ * @n: new node value
  * Return: elements of the list
  */
-size_t dlistint_len(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if ((*head) == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (*head);
+	}
+	while ((*head)->prev)
+		*head = (*head)->prev;
+	(*head)->prev = new;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (*head);
 }
