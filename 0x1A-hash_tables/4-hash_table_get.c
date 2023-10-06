a2276027b7b552cc72b9f2a12a339b1c0b39fe06
AbduGad <abdo79gad@gmail.com>
2023-10-06 20:05:53 +0300

    added check for key in 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *holder;
 
+	if (!key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	holder = ht->array[index];
 	while (holder && strcmp(holder->key, key))
