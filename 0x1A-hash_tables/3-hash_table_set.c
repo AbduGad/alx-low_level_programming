db4ee1f5af7ff0b89c728b92c56a4ef8917bcd11
AbduGad <abdo79gad@gmail.com>
2023-10-06 18:05:05 +0300

    task 3 test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		write(2, "key cannot be an empty string", 30);
 		return (0);
 	}
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	check = ht->array[index];
 
 	while (check)
@@ -44,4 +44,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	check->next = NULL;
+
+	return (1);
 }
