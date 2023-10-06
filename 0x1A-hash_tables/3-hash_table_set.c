1e4b01ad52d3ce2237c41689cfae705be5b3a9ea
AbduGad <abdo79gad@gmail.com>
2023-10-06 19:10:31 +0300

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	holder = ht->array[index];
 
 	while (holder)
-			holder = holder->next;
+		holder = holder->next;
 
 	/*printf("here\n");*/
 	holder = malloc(sizeof(hash_node_t));
