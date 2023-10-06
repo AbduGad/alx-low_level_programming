59fb7d07baf7340ab6b5ac9d9c9fed036229208d
AbduGad <abdo79gad@gmail.com>
2023-10-06 11:05:38 +0300

    was not allocating pointers

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int i;
+	hash_table_t *table = NULL;
+	/*unsigned int i;*/
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	if (size == 0)
 		return (NULL);
-
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
+	/*for (i = 0; i < size; i++)*/
+		/*table->array[i] = NULL;*/
 
-	return (new_table);
+	return (table);
 }
