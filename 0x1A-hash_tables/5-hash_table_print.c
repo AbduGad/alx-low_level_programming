b03cb8a45410a16e3e2f64985ac46339dcaba8bf
AbduGad <abdo79gad@gmail.com>
2023-10-06 20:58:27 +0300

    test 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i]->next)
 		{
 			ht->array[i] = ht->array[i]->next;
-			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
+			/*printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);*/
 		}
 		else
 			i++;
